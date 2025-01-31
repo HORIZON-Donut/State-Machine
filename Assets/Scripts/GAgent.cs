8e1c17d19f5471f0e2a7ce37fdb3851672ecb423
HORIZON-Donut <kiettisak.k2546@gmail.com>
2025-01-31 15:00:31 +0700

    Define bunch of variable

--- Assets/Scripts/GAgent.cs
+++ Assets/Scripts/GAgent.cs
@@ -1,7 +1,27 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class GAgent : MonoBehaviour
+public abstract class GAgent : MonoBehaviour
 {
+    public string actionName = "Action";
+    public float cost = 1.0f;
+    public float duration = 0;
+
+    public GameObjsct target;
+    public GameObject targetTag;
+
+    public WorldState[] preCondition;
+    public WorldState[] afterEffect;
+
+    public NavMeshAgent agent;
+
+    public Dictionary<string, int> preconditions;
+    public Dictionary<string, int> effects;
+
+    public WorldStates agentBeliefs;
+
+    public bool running = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
