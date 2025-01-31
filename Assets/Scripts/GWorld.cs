128ecba7c8a22474ef3fdf48a65a4cd8bc032dcc
HORIZON-Donut <kiettisak.k2546@gmail.com>
2025-01-31 14:43:38 +0700

    Add stuff to g world script

--- Assets/Scripts/GWorld.cs
+++ Assets/Scripts/GWorld.cs
@@ -1,7 +1,27 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public sealed class GWorld
 {
     private static readonly GWorld instance = new GWorld();
     private static WorldState world;
+
+    static GWorld()
+    {
+        world = new WorldState();
+    }
+
+    private GWorld()
+    {
+    }
+
+    public static GWorld Instance
+    {
+        get { return instance};
+    }
+
+    public WorldState GetWorld()
+    {
+        return world;
+    }
 }
