d6a6d8a86cfc35cd9263b65d4131fdc118fcb896
HORIZON-Donut <kiettisak.k2546@gmail.com>
2025-02-06 04:06:59 +0700

    Add and modified some stuff in patient script

--- Assets/Scripts/Patient.cs
+++ Assets/Scripts/Patient.cs
@@ -1,16 +1,11 @@
 using UnityEngine;
 
-public class Patient : MonoBehaviour
+public class Patient : GAgent
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        base.Start();
+        SubGoal s1 = new SubGoal("isWaiting", 1, true);
+        goals.Add(s1, 3);
     }
 }
