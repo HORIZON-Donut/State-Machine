ab5e8f6f54eedc4a8a228a8bce8ac867562ec754
HORIZON-Donut <kiettisak.k2546@gmail.com>
2025-02-06 01:05:33 +0700

    Add node class to script

--- Assets/Scripts/GPlanner.cs
+++ Assets/Scripts/GPlanner.cs
@@ -1,4 +1,22 @@
 using UnityEngine;
+using System.Linq;
+using System.Collections.Generic;
+
+public class Node
+{
+    public Node parent;
+    public float cost;
+    public Dictionary<string, int> state;
+    public GAction action;
+
+    public Node(Node parent, float cost, Dictionary<string, int> state, GAction action)
+    {
+        this.parent = parent;
+        this.cost = cost;
+        this.state = new Dictionary<string, int>(state);
+        this.action = action;
+    }
+}
 
 public class GPlanner : MonoBehaviour
 {
