cea46f61a924e3c20e0acbe3a4f6ef36d44bc225
HORIZON-Donut <kiettisak.k2546@gmail.com>
2025-02-06 01:06:23 +0700

    Prepare g planner class

--- Assets/Scripts/GPlanner.cs
+++ Assets/Scripts/GPlanner.cs
@@ -18,17 +18,5 @@ public class Node
     }
 }
 
-public class GPlanner : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+public class GPlanner
 }
